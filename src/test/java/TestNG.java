b8a57bf5c6d0c3e3c413c56b6ae00679f60b61bd
Prabhath <97177047+Tprabath@users.noreply.github.com>
2026-09-14 00:42:00 +0530

    Refactor enums; add locators & assertions
    Rename HASHMAP_KEYS to WEB_ELEMENT_LOCATOR_KEYS and introduce CHECKOUT_DATA_KEYS. Add WEB_ELEMENT_LOCATORS and EXPECTED_VALUES maps and migrate EXAMPLE_CHECKOUT_DATA to use CHECKOUT_DATA_KEYS. Add org.openqa.selenium.By import and a navbar text assertion + log in homePageSmokeTest. Improves separation of locators, expected values and test data, and adds TC01 UI verification.

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -6,10 +7,13 @@ import java.util.HashMap;
 
 public class TestNG extends BaseTest {
 
-    private enum HASHMAP_KEYS {
+    private enum WEB_ELEMENT_LOCATOR_KEYS {
         // web element locators keys
+        NAVBAR_ID,
 
+    }
 
+    private enum CHECKOUT_DATA_KEYS {
         // checkout data keys
         CHECKOUT_DATA_NAME,
         CHECKOUT_DATA_COUNTRY,
@@ -20,20 +24,39 @@ public class TestNG extends BaseTest {
     }
 
 
-    private static final HashMap<HASHMAP_KEYS,String> WEB_ELEMENT_LOCATORS = new HashMap<>();
-    private static final HashMap<HASHMAP_KEYS,Object> EXAMPLE_CHECKOUT_DATA = new HashMap<>();
+    /*
+     * a map for web element locators
+     */
+    private static final HashMap<WEB_ELEMENT_LOCATOR_KEYS,String>
+            WEB_ELEMENT_LOCATORS = new HashMap<>();
+
+    /*
+    * a map for expected values,
+    * for map WEB_ELEMENT_LOCATOR = expected value
+     */
+    private static final HashMap<WEB_ELEMENT_LOCATOR_KEYS,String>
+            EXPECTED_VALUES = new HashMap<>();
+
+    /*
+    * a map for store example checkout data
+     */
+    private static final HashMap<CHECKOUT_DATA_KEYS,Object>
+            EXAMPLE_CHECKOUT_DATA = new HashMap<>();
 
     static {
         //init web element locators
+        WEB_ELEMENT_LOCATORS.put(WEB_ELEMENT_LOCATOR_KEYS.NAVBAR_ID,"nava");
 
+        //init expected values
+        EXPECTED_VALUES.put(WEB_ELEMENT_LOCATOR_KEYS.NAVBAR_ID,"PRODUCT STORE");
 
         // init example checkout data
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_NAME,"Test Student");
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_COUNTRY, "Sri Lanka");
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_CITY, "Colombo");
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_CREDIT_CARD_NO, "4111111111111111");
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_MONTH, 12);
-        EXAMPLE_CHECKOUT_DATA.put(HASHMAP_KEYS.CHECKOUT_DATA_YEAR, 2027);
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_NAME,"Test Student");
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_COUNTRY, "Sri Lanka");
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_CITY, "Colombo");
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_CREDIT_CARD_NO, "4111111111111111");
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_MONTH, 12);
+        EXAMPLE_CHECKOUT_DATA.put(CHECKOUT_DATA_KEYS.CHECKOUT_DATA_YEAR, 2027);
     }
 
     @BeforeMethod
@@ -47,7 +70,13 @@ public class TestNG extends BaseTest {
 
     @Test //TC01
     public void homePageSmokeTest(){
-
+        assert driver.findElement(
+                    By.id(WEB_ELEMENT_LOCATORS.get(WEB_ELEMENT_LOCATOR_KEYS.NAVBAR_ID))
+                ).getText()
+                .equals(EXPECTED_VALUES.get(WEB_ELEMENT_LOCATOR_KEYS.NAVBAR_ID))
+                : "Title is empty";
+        
+        log("TC01 : Home Page Smoke Test PASS");
     }
 
     @Test(enabled = false) //TC02
